80771c7d19ca7e3aba114b716bd59e8aa8250984
interdimensionalpizza <michaellake05@gmail.com>
2023-06-05 14:59:07 -0700

    finished challenge

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,15 @@
-function bottleSong() {
-  // code goes here okay okay
+function bottleSong(num) {
+  for (let i = num; i > 2; i--) {
+    console.log(i + ' bottles of beer on the wall, ' + i + ' bottles of beer.')
+    console.log('Take one down and pass it around, ' + (i-1) + ' bottles of beer on the wall.') 
+  }  
+  console.log('2 bottles of beer on the wall, 2 bottles of beer.')
+  console.log('Take one down and pass it around, 1 bottle of beer on the wall.')
+  console.log('1 bottle of beer on the wall, 1 bottle of beer.')
+  console.log('Take one down and pass it around, no more bottles of beer on the wall.')
+  console.log('No more bottles of beer on the wall, no more bottles of beer.')
+  console.log('Go to the store and buy some more, ' + num + ' bottles of beer on the wall.')
 };
 
-bottleSong();
+
+bottleSong(99);
