1be7afc89d1b652b0cf453d73a7dc2ef0f221b96
interdimensionalpizza <michaellake05@gmail.com>
2023-06-08 11:06:21 -0700

    finished python version

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,7 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+	while num > 2:
+		print(f"{num} bottles of beer on the wall, {num} bottles of beer.\nTake one down and pass it around, {num - 1} bottles of beer on the wall.")
+		num -= 1
+	print(f"2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.")
 
-
-bottle_song()
+bottle_song(99)
